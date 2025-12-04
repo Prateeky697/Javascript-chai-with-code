952120b4caae291f6a11c3991a2a30b1c1536a27
Prateek yadav <71202336+Prateeky697@users.noreply.github.com>
2025-12-04 19:05:42 +0530

    Add video_no_6.js for datatype conversion example

---
+++ video_no_6.js
@@ -0,0 +1,9 @@
+//Datatype conversion confusion | chai aur #javascript
+let score = "100";
+console.log("Video No. 6");
+console.log(typeof score); //string
+
+//Converting string to number
+let convertedScore = Number(score);
+console.log(convertedScore); //100
+console.log(typeof convertedScore); //number
