ad58b00e6fabf9131ca2152fc2922a53dd30557b
Prateek yadav <71202336+Prateeky697@users.noreply.github.com>
2025-12-04 18:53:45 +0530

    Create video_no_5.js for JavaScript datatypes
    Add video_no_5.js demonstrating datatypes in JavaScript

---
+++ video_no_5.js
@@ -0,0 +1,16 @@
+//Datatypes and ECMA standards | chai aur #javascript
+// alert(3+3) thi is not run your machine its run in browser console
+console.log("Video No. 5");
+let firstName = "Prateek"; //string literal
+let lastName = "Yadav"; //string literal
+let age = 21; //number literal
+let isLoggedIn = false; //boolean literal
+let state; //undefined
+let nullVar = null; //null
+
+console.log(typeof firstName);
+console.log(typeof lastName);
+console.log(typeof age);
+console.log(typeof isLoggedIn);
+console.log(typeof state);
+console.log(typeof nullVar); //object (this is a known bug in JavaScript)   
