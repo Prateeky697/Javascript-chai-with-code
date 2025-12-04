4b545eaf01103c51544272cf15ba41863c36c5fa
Prateek yadav <71202336+Prateeky697@users.noreply.github.com>
2025-12-04 18:38:39 +0530

    Refactor variable declarations with comments
    Updated variable declarations with comments explaining their types and usage.

--- video_No_4.js
+++ video_No_4.js
@@ -1,7 +1,7 @@
 console.log("video no 4")
-const Accountid = 123
-let accountEmail = "prateekyadav"
-var accountPassword = 123
+const Accountid = 123   //this is const data type value it is not changable
+let accountEmail = "prateekyadav" // this a type of method of the diclaration the variable its new type of style of use the method its look working a block
+var accountPassword = 123  // it is same think use declear the variable but old method javascript not use now becouse global and changeble
 accountCity = "jaipur"
 
 
