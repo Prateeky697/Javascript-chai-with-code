83f0641013d299c42718a1c467fe2ff6d2be2c1b
Prateek yadav <71202336+Prateeky697@users.noreply.github.com>
2025-12-04 18:32:44 +0530

    Add video_No_4.js with account details logging

---
+++ video_No_4.js
@@ -0,0 +1,14 @@
+console.log("video no 4")
+const Accountid = 123
+let accountEmail = "prateekyadav"
+var accountPassword = 123
+accountCity = "jaipur"
+
+
+console.log(Accountid)
+
+console.log(accountEmail)
+
+console.log(accountPassword)
+
+console.log(accountCity)        
