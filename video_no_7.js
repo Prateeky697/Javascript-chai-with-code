a3b3c37d58e6e567078268975d66aa45bd7486ca
Prateek yadav <71202336+Prateeky697@users.noreply.github.com>
2025-12-05 10:31:40 +0530

    Add video_no_7.js with string to number examples

---
+++ video_no_7.js
@@ -0,0 +1,10 @@
+//Why string to number conversion is confusing | chai aur #javascript
+console.log(1+"1")
+console.log("1"+2)
+console.log(1+2+"3")
+console.log(true)
+console.log(true+1)
+console.log(-true+1)
+console.log(++ture)
+console.log(true++)
+
