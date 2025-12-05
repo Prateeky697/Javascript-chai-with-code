818c109e7afd1f4940e0e18878c7bdc98e349295
Prateek yadav <71202336+Prateeky697@users.noreply.github.com>
2025-12-05 14:03:49 +0530

    Create video_no_9.js for JavaScript data types
    Added a summary of JavaScript data types with examples.

---
+++ video_no_9.js
@@ -0,0 +1,29 @@
+// Data types of javascript summary | chai aur #javascript
+let name="prateek";             
+let age=21;                     
+let isLoggedIn=true;             
+let firstName;                 
+let nullVar=null;                 
+let sym=Symbol("my symbol");   
+let sym1=Symbol("my symbol  ")  
+console.log(sym===sym1) // symbol not same
+
+console.log(typeof name);        
+console.log(typeof age);         
+console.log(typeof isLoggedIn);  
+console.log(typeof firstName);   
+console.log(typeof nullVar);     
+console.log(typeof sym);         
+let arr=[1,2,3,4];               
+console.log(typeof arr);         
+let obj={
+    name:"prateek",
+    age:21,
+    isLoggedIn:true
+};                                // object
+console.log(typeof obj);         // object
+function greet(){
+    console.log("hello world");
+}                                
+console.log(typeof greet);   
+console.log(greet());       // function  
