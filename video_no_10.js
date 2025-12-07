ad0cfd1969a8e9cc3291815bbd708f67d911553d
Prateek yadav <71202336+Prateeky697@users.noreply.github.com>
2025-12-07 13:50:08 +0530

    Create video_no_10.js

---
+++ video_no_10.js
@@ -0,0 +1,17 @@
+// Stack and Heap memory in javascript
+let name="prateek yadav"
+let name2=name;
+console.log("no change before "+name)
+console.log("no change before "+name2)
+name2="alok yadav"
+console.log("no change before "+name)
+console.log("no change before "+name2)
+//  checked the non primitive value
+let student={
+    name="prateek";
+    age=12;
+    isloggdin=false
+
+}
+console.log(student);
+let student2=student;
