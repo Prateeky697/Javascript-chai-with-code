7f04b9c85136467a091d853b5725a9b8f54248ae
Prateek yadav <71202336+Prateeky697@users.noreply.github.com>
2025-12-07 14:17:01 +0530

    Fix length method calls for balence conversions

--- video_No_12.js
+++ video_No_12.js
@@ -3,3 +3,5 @@ const name=23
 console.log(name)
 const balence=new Number(100)
 console.log(balence)
+console.log(balence.toString().length())// change to string 
+console.log(balence.toFixed(2).length())// change to Number
